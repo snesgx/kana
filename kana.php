ea1a9e7b32c5cd611e27b1b9a81b68d113a5187c
SnesGX OldAnkkes <snesgx@oldankkes>
2022-04-24 00:38:31 -0400

    Adding input for checking

--- kana.php
+++ kana.php
@@ -4,6 +4,19 @@ echo "Guess the text";
 
 <p id="txtJapan" style="font-size:50px;"></p>
 
+<input id="txtAnswer" value="Your answer...">
+<button id="btnCheck" onclick="checkAnswer()">Button</button>
+
+<script>
+var input = document.getElementById("txtAnswer");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("btnCheck").click();
+  }
+});
+</script>
+
 <script>
 var kana = 
 { a: "あア", i: "いイ", u: "うウ", e: "えエ", o: "おオ",
@@ -13,5 +26,31 @@ var kana =
   na:"なナ", ni:"にニ", nu:"ぬヌ", ne:"ねネ", no: "のノ",
   ha:"はハ", hi:"ひヒ", hu:"ふフ", he:"へヘ", ho: "ほホ"};
 
-document.getElementById("txtJapan").innerHTML = kana[Object.keys(kana)[Math.floor(Math.random() * Object.keys(kana).length)]];
+	var lastWordRomaji="";
+	var lastWordKana="";  
+  
+function GenerateQuery(){
+	lastWordRomaji="";
+	lastWordKana="";
+	var HiraOrKata = Math.floor(Math.random() * 2);
+	
+	for (let i = 0; i < Math.floor(Math.random() * 6)+1; i++) {
+		var nextSyllabe = Object.keys(kana)[Math.floor(Math.random() * Object.keys(kana).length)];
+		lastWordRomaji += nextSyllabe;
+		lastWordKana += kana[nextSyllabe][HiraOrKata];
+	}
+	
+	document.getElementById("txtJapan").innerHTML = lastWordKana;
+}
+
+function checkAnswer() {
+	console.log(lastWordRomaji);
+	console.log(document.getElementById("txtAnswer").value);
+}
+
+GenerateQuery();
+
+
+
 </script>
+
